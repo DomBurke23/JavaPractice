518987628bdd3d73f2ffc12bccb34e2bab0357a9
DomBurke23 <dominiqueburke@hotmail.co.uk>
2021-03-25 13:13:26 +0000

    Update CalculateTotal.java
    ArrayList further practice. 
    Added clearing an ArrayList , modifying an entry, removing an entry, getting the size of the ArrayList . Also added a for loop to order the ArrayList into ascending order.

--- CalculateTotal.java
+++ CalculateTotal.java
@@ -1,23 +1,46 @@
+// import array list library. 
 import java.util.ArrayList;
+import java.util.Collections; 
+
+/* elements can be added and removed from an ArrayList whenever you want */
 
 class CalculateTotal {
   
   public static void main(String[] args) {
-    
+    // Create an ArrayList object and add elements. 
     ArrayList<Double> expenses = new ArrayList<Double>();
+    //clear list before adding elements 
+    expenses.clear();
+    // add elements
     expenses.add(74.46);
     expenses.add(63.99);
     expenses.add(10.57);
     expenses.add(81.37);
     
+    // Modify an entry
+    expenses.set(2, 10.58);
+    // Remove an entry
+    expenses.remove(3);
+    
     double total = 0;
     
-    // Iterate over expenses
+    // Get size of Array list 
+    int ArrayListSize = expenses.size(); 
+    
+    // Iterate over expenses, expenses.size access the elements in the list. 
     for (int i = 0; i<expenses.size();i++){
       total = total+expenses.get(i);
     }
     
+    // print the arraylist in ascending order. 
+	  Collections.sort(expenses);
+	  for (double i : expenses) {
+      System.out.println(i);
+    }
+    
     System.out.println(total);
+	  System.out.println("Size of ArrayList " + ArrayListSize);
+    System.out.println("Size of ArrayList " + expenses.size());
     
   }
   
