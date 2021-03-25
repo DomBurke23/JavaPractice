350621bf5d62dea45bbe012db2dce1e202c47d45
DomBurke23 <dominiqueburke@hotmail.co.uk>
2021-03-25 14:05:50 +0000

    Update CalculateTotal.java
    User input to add their own expense to ArrayList.

--- CalculateTotal.java
+++ CalculateTotal.java
@@ -1,6 +1,7 @@
 // import array list library. 
 import java.util.ArrayList;
 import java.util.Collections; 
+import java.util.Scanner;
 
 /* elements can be added and removed from an ArrayList whenever you want */
 
@@ -21,6 +22,10 @@ class CalculateTotal {
     expenses.set(2, 10.58);
     // Remove an entry
     expenses.remove(3);
+	  
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter your expense: ");
+    expenses.add(sc.nextDouble());
     
     double total = 0;
     
@@ -31,15 +36,16 @@ class CalculateTotal {
     for (int i = 0; i<expenses.size();i++){
       total = total+expenses.get(i);
     }
-    
+	  
+    System.out.println("Your ordered list: ");    
     // print the arraylist in ascending order. 
 	  Collections.sort(expenses);
 	  for (double i : expenses) {
       System.out.println(i);
     }
     
-    System.out.println(total);
-	  System.out.println("Size of ArrayList " + ArrayListSize);
+    System.out.println("Total expense = " + total);
+    System.out.println("Size of ArrayList " + ArrayListSize);
     System.out.println("Size of ArrayList " + expenses.size());
     
   }
